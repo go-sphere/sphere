e9e17d6726e0556cafe4cda62afb13f3ec3c6ee5
tbxark <tbxark@outlook.com>
2024-06-26 10:27:49 +0800

    perf: 缓存添加编码解码器

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -7,10 +7,6 @@ import (
 	"time"
 )
 
-type Encoder interface {
-	Encode(val any) ([]byte, error)
-}
-
 type Cache[S any] interface {
 	Set(ctx context.Context, key string, val S, expiration time.Duration) error
 	Get(ctx context.Context, key string) (S, error)
@@ -26,29 +22,49 @@ func NewCache() ByteCache {
 	return NewMemoryCache()
 }
 
-func LoadJson[T any](ctx context.Context, c ByteCache, key string) (*T, error) {
+type Encoder interface {
+	Marshal(val any) ([]byte, error)
+}
+
+type EncoderFunc func(val any) ([]byte, error)
+
+func (e EncoderFunc) Marshal(val any) ([]byte, error) {
+	return e(val)
+}
+
+type Decoder interface {
+	Unmarshal(data []byte, val any) error
+}
+
+type DecoderFunc func(data []byte, val any) error
+
+func (d DecoderFunc) Unmarshal(data []byte, val any) error {
+	return d(data, val)
+}
+
+func Load[T any, D Decoder](ctx context.Context, c ByteCache, d D, key string) (*T, error) {
 	data, err := c.Get(ctx, key)
 	if err != nil {
 		return nil, err
 	}
 	var value T
-	err = json.Unmarshal(data, &value)
+	err = d.Unmarshal(data, &value)
 	if err != nil {
 		return nil, err
 	}
 	return &value, nil
 }
 
-func SaveJson[T any](ctx context.Context, c ByteCache, key string, value *T, expiration time.Duration) error {
-	data, err := json.Marshal(value)
+func Save[T any, E Encoder](ctx context.Context, c ByteCache, e E, key string, value *T, expiration time.Duration) error {
+	data, err := e.Marshal(value)
 	if err != nil {
 		return err
 	}
 	return c.Set(ctx, key, data, expiration)
 }
 
-func LoadJsonEx[T any](ctx context.Context, c ByteCache, sf *singleflight.Group, key string, expiration time.Duration, builder func() (obj *T, err error)) (*T, error) {
-	obj, err := LoadJson[T](ctx, c, key)
+func LoadEx[T any, D Decoder, E Encoder](ctx context.Context, c ByteCache, d D, e E, sf *singleflight.Group, key string, expiration time.Duration, builder func() (obj *T, err error)) (*T, error) {
+	obj, err := Load[T, D](ctx, c, d, key)
 	if err == nil {
 		return obj, nil
 	}
@@ -57,7 +73,7 @@ func LoadJsonEx[T any](ctx context.Context, c ByteCache, sf *singleflight.Group,
 		if nErr != nil {
 			return nil, nErr
 		}
-		nErr = SaveJson[T](ctx, c, key, nObj, expiration)
+		nErr = Save[T, E](ctx, c, e, key, nObj, expiration)
 		if nErr != nil {
 			return nil, nErr
 		}
@@ -67,4 +83,16 @@ func LoadJsonEx[T any](ctx context.Context, c ByteCache, sf *singleflight.Group,
 		return nil, err
 	}
 	return build.(*T), nil
+}
+
+func LoadJson[T any](ctx context.Context, c ByteCache, key string) (*T, error) {
+	return Load[T, DecoderFunc](ctx, c, json.Unmarshal, key)
+}
+
+func SaveJson[T any](ctx context.Context, c ByteCache, key string, value *T, expiration time.Duration) error {
+	return Save[T, EncoderFunc](ctx, c, json.Marshal, key, value, expiration)
+}
+
+func LoadJsonEx[T any](ctx context.Context, c ByteCache, sf *singleflight.Group, key string, expiration time.Duration, builder func() (obj *T, err error)) (*T, error) {
+	return LoadEx[T, DecoderFunc, EncoderFunc](ctx, c, json.Unmarshal, json.Marshal, sf, key, expiration, builder)
 }
