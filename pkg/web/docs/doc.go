1002711fcfc7d50d5d067489c66abb2b6e8d28df
tbxark <tbxark@outlook.com>
2024-10-17 10:50:51 +0800

    fix: gitignore

---
+++ pkg/web/docs/doc.go
@@ -0,0 +1,20 @@
+package docs
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag"
+	"net/http"
+)
+
+func SetupDoc(doc *swag.Spec, title string, route gin.IRoutes) {
+	doc.Title = title
+	route.GET("/swagger-raw/doc.json", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, doc.ReadDoc())
+	})
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(
+		swaggerFiles.Handler,
+		ginSwagger.URL("/swagger-raw/doc.json"),
+	))
+}
