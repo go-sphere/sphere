a14a4686ef8cd5bedd5b6effefd73d42f7bf55d8
tbxark <tbxark@outlook.com>
2024-10-23 18:26:24 +0800

    perf: 简化strParser函数

--- pkg/server/middleware/auth/parser.go
+++ pkg/server/middleware/auth/parser.go
@@ -7,6 +7,44 @@ import (
 )
 
 func strParser(kind reflect.Kind) func(string) (any, error) {
+	parseInt := func(str string, bitSize int) (any, error) {
+		num, err := strconv.ParseInt(str, 10, bitSize)
+		if err != nil {
+			return nil, err
+		}
+		switch bitSize {
+		case 8:
+			return int8(num), nil
+		case 16:
+			return int16(num), nil
+		case 32:
+			return int32(num), nil
+		case 64:
+			return num, nil
+		default:
+			return int(num), nil
+		}
+	}
+
+	parseUint := func(str string, bitSize int) (any, error) {
+		num, err := strconv.ParseUint(str, 10, bitSize)
+		if err != nil {
+			return nil, err
+		}
+		switch bitSize {
+		case 8:
+			return uint8(num), nil
+		case 16:
+			return uint16(num), nil
+		case 32:
+			return uint32(num), nil
+		case 64:
+			return num, nil
+		default:
+			return uint(num), nil
+		}
+	}
+
 	switch kind {
 	case reflect.String:
 		return func(str string) (any, error) {
@@ -14,83 +52,43 @@ func strParser(kind reflect.Kind) func(string) (any, error) {
 		}
 	case reflect.Int:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseInt(str, 10, strconv.IntSize)
-			if err != nil {
-				return nil, err
-			}
-			return int(num), nil
+			return parseInt(str, strconv.IntSize)
 		}
 	case reflect.Int8:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseInt(str, 10, 8)
-			if err != nil {
-				return nil, err
-			}
-			return int8(num), nil
+			return parseInt(str, 8)
 		}
 	case reflect.Int16:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseInt(str, 10, 16)
-			if err != nil {
-				return nil, err
-			}
-			return int16(num), nil
+			return parseInt(str, 16)
 		}
 	case reflect.Int32:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseInt(str, 10, 32)
-			if err != nil {
-				return nil, err
-			}
-			return int32(num), nil
+			return parseInt(str, 32)
 		}
 	case reflect.Int64:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseInt(str, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			return num, nil
+			return parseInt(str, 64)
 		}
 	case reflect.Uint:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseUint(str, 10, strconv.IntSize)
-			if err != nil {
-				return nil, err
-			}
-			return uint(num), nil
+			return parseUint(str, strconv.IntSize)
 		}
 	case reflect.Uint8:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseUint(str, 10, 8)
-			if err != nil {
-				return nil, err
-			}
-			return uint8(num), nil
+			return parseUint(str, 8)
 		}
 	case reflect.Uint16:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseUint(str, 10, 16)
-			if err != nil {
-				return nil, err
-			}
-			return uint16(num), nil
+			return parseUint(str, 16)
 		}
 	case reflect.Uint32:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseUint(str, 10, 32)
-			if err != nil {
-				return nil, err
-			}
-			return uint32(num), nil
+			return parseUint(str, 32)
 		}
 	case reflect.Uint64:
 		return func(str string) (any, error) {
-			num, err := strconv.ParseUint(str, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			return num, nil
+			return parseUint(str, 64)
 		}
 	default:
 		return func(str string) (any, error) {
