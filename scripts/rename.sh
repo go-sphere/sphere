ca9f28aa7ac9448b3278b7e9f9e4dde4a4fc8261
tbxark <tbxark@outlook.com>
2024-10-29 17:32:23 +0800

    fix: rename 脚本bug

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -3,19 +3,20 @@ set -e
 
 OLD_MODULE=$(go list -m)
 read -r -p "Enter new module name: " NEW_MODULE
-read -r -p "Do you want to keep pkg module name as ${OLD_MODULE} (y/n)? " KEEP_PKG_NAME
+read -r -p "Do you want to keep pkg module name as ${OLD_MODULE}/pkg (y/n)? " KEEP_PKG_NAME
 
 echo "Renaming module from ${OLD_MODULE} to ${NEW_MODULE}"
 
-go mod edit -module "${NEW_MODULE}"
-find . -type f -name '*.go' -exec sed -i -e "s,${OLD_MODULE},${NEW_MODULE},g" {} \;
-find . -type f -name '*.go' -exec sed -i -e "s,${OLD_MODULE}/cmd,${NEW_MODULE}/cmd,g" {} \;
-find . -type f -name '*.go' -exec sed -i -e "s,${OLD_MODULE}/internal,${NEW_MODULE}/internal,g" {} \;
+
+find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE},${NEW_MODULE},g" {} \;
+find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/cmd,${NEW_MODULE}/cmd,g" {} \;
+find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/internal,${NEW_MODULE}/internal,g" {} \;
 if [ "${KEEP_PKG_NAME}" == "n" ]; then
-  find . -type f -name '*.go' -exec sed -i -e "s,${OLD_MODULE}/pkg,${NEW_MODULE}/pkg,g" {} \;
+  find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/pkg,${NEW_MODULE}/pkg,g" {} \;
 else
   rm -rf pkg
 fi
+go mod edit -module "${NEW_MODULE}"
 
-echo "Update buf.gem.yaml"
-sed -i -e "s,${OLD_MODULE},${NEW_MODULE},g" buf.gem.yaml
+echo "Update buf.gen.yaml"
+sed -i '' "s,${OLD_MODULE},${NEW_MODULE},g" buf.gen.yaml
