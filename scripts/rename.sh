e5c566ae517d83132fc56f911cce3d92e534a7a6
tbxark <tbxark@outlook.com>
2024-11-07 14:33:05 +0800

    chore: 优化重命名脚本

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -2,21 +2,29 @@
 set -e 
 
 OLD_MODULE=$(go list -m)
+if [ "${OLD_MODULE}" != "github.com/tbxark/sphere" ]; then
+  echo "Current module is not github.com/tbxark/sphere, please run this script in the root of Sphere"
+  exit 1
+fi
+
 read -r -p "Enter new module name: " NEW_MODULE
-read -r -p "Do you want to keep pkg module name as ${OLD_MODULE}/pkg (y/n)? " KEEP_PKG_NAME
+if [ -z "${NEW_MODULE}" ]; then
+  echo "New module name is required"
+  exit 1
+fi
+
+make init
+
+rm -rf ./pkg
+rm -rf ./contrib
 
 echo "Renaming module from ${OLD_MODULE} to ${NEW_MODULE}"
-
-
 find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE},${NEW_MODULE},g" {} \;
 find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/cmd,${NEW_MODULE}/cmd,g" {} \;
 find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/internal,${NEW_MODULE}/internal,g" {} \;
-if [ "${KEEP_PKG_NAME}" == "n" ]; then
-  find . -type f -name "*.go" -exec sed -i '' "s,${OLD_MODULE}/pkg,${NEW_MODULE}/pkg,g" {} \;
-else
-  rm -rf pkg
-fi
 go mod edit -module "${NEW_MODULE}"
 
 echo "Update buf.gen.yaml"
 sed -i '' "s,${OLD_MODULE},${NEW_MODULE},g" buf.gen.yaml
+
+make generate
