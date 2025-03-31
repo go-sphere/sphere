bb96734812a09022fc3a93e8acf224ea1c3c8084
tbxark <tbxark@outlook.com>
2025-03-31 10:04:23 +0800

    refactor(auth): simplify auth middleware logic and enhance error handling
    Signed-off-by: tbxark <tbxark@outlook.com>

--- server/middleware/auth/middleware.go
+++ server/middleware/auth/middleware.go
@@ -16,54 +16,68 @@ type AccessControl interface {
 	IsAllowed(role, resource string) bool
 }
 
-func NewAuthMiddleware(prefix string, parser authorizer.Parser[authorizer.RBACClaims[int64]], abortOnError bool) gin.HandlerFunc {
-	abort := func(ctx *gin.Context) {
-		if abortOnError {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"message": "unauthorized",
-			})
-		}
+func abort(ctx *gin.Context, abortOnError bool) {
+	if abortOnError {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "unauthorized",
+		})
 	}
+}
 
+func parserToken(ctx *gin.Context, raw string, loader func(text string) (string, error), parser authorizer.Parser[authorizer.RBACClaims[int64]], abortOnError bool) {
+	token, err := loader(raw)
+	if err != nil {
+		abort(ctx, abortOnError)
+		return
+	}
+	claims, err := parser.ParseToken(token)
+	if err != nil {
+		abort(ctx, abortOnError)
+		return
+	}
+	ctx.Set(authorizer.ContextKeyUID, claims.UID)
+	ctx.Set(authorizer.ContextKeySubject, claims.Subject)
+	ctx.Set(authorizer.ContextKeyRoles, claims.Roles)
+}
+
+func NewAuthMiddleware(prefix string, parser authorizer.Parser[authorizer.RBACClaims[int64]], abortOnError bool) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader(AuthorizationHeader)
 		if token == "" {
-			abort(ctx)
+			abort(ctx, abortOnError)
 			return
 		}
+		parserToken(ctx, token, func(text string) (string, error) {
+			if len(prefix) > 0 && strings.HasPrefix(token, prefix+" ") {
+				token = strings.TrimSpace(strings.TrimPrefix(token, prefix+" "))
+			}
+			return token, nil
+		}, parser, abortOnError)
+	}
+}
 
-		if len(prefix) > 0 && strings.HasPrefix(token, prefix+" ") {
-			token = token[len(prefix)+1:]
-		}
-
-		claims, err := parser.ParseToken(token)
+func NewCookieAuthMiddleware(cookieName string, loader func(raw string) (string, error), parser authorizer.Parser[authorizer.RBACClaims[int64]], abortOnError bool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token, err := ctx.Cookie(cookieName)
 		if err != nil {
-			abort(ctx)
+			abort(ctx, abortOnError)
 			return
 		}
-
-		ctx.Set(authorizer.ContextKeyUID, claims.UID)
-		ctx.Set(authorizer.ContextKeySubject, claims.Subject)
-		ctx.Set(authorizer.ContextKeyRoles, claims.Roles)
+		parserToken(ctx, token, loader, parser, abortOnError)
 	}
 }
 
 func NewPermissionMiddleware(resource string, acl AccessControl) gin.HandlerFunc {
-	abort := func(ctx *gin.Context) {
-		ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{
-			"message": "forbidden",
-		})
-	}
 	return func(ctx *gin.Context) {
 		rolesRaw, exist := ctx.Get(authorizer.ContextKeyRoles)
 		if !exist {
-			abort(ctx)
+			abort(ctx, true)
 			return
 		}
 
 		roles, ok := rolesRaw.([]string)
 		if !ok {
-			abort(ctx)
+			abort(ctx, true)
 			return
 		}
 
@@ -72,6 +86,6 @@ func NewPermissionMiddleware(resource string, acl AccessControl) gin.HandlerFunc
 				return
 			}
 		}
-		abort(ctx)
+		abort(ctx, true)
 	}
 }
