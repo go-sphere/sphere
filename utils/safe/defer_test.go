23ebcaa319c91968f8b3cd9a591532e28d067141
tbxark <tbxark@outlook.com>
2025-05-09 15:26:30 +0800

    test: add unit tests for IfErrorPresent function
    Signed-off-by: tbxark <tbxark@outlook.com>

---
+++ utils/safe/defer_test.go
@@ -0,0 +1,31 @@
+package safe
+
+import (
+	"testing"
+)
+
+func TestDeferIfErrorPresent(t *testing.T) {
+	t.Logf("1")
+	defer IfErrorPresent("test", func() error {
+		t.Logf("5")
+		return nil
+	})
+	t.Logf("2")
+	defer IfErrorPresent("test", func() error {
+		t.Logf("4")
+		return nil
+	})
+	t.Logf("3")
+}
+
+func TestDefer2IfErrorPresent(t *testing.T) {
+	t.Logf("1")
+	defer func() {
+		t.Logf("5")
+	}()
+	t.Logf("2")
+	defer func() {
+		t.Logf("4")
+	}()
+	t.Logf("3")
+}
