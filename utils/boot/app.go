19122eb39774e1b8b82d7e84d625db1a78110d5c
tbxark <tbxark@outlook.com>
2025-05-15 17:38:38 +0800

    feat: implement task manager for concurrent task execution and management
    Signed-off-by: tbxark <tbxark@outlook.com>

--- utils/boot/app.go
+++ utils/boot/app.go
@@ -3,10 +3,6 @@ package boot
 import (
 	"context"
 	"fmt"
-
-	"github.com/TBXark/sphere/log"
-	"github.com/TBXark/sphere/log/logfields"
-	"golang.org/x/sync/errgroup"
 )
 
 type Task interface {
@@ -16,51 +12,15 @@ type Task interface {
 }
 
 type Application struct {
-	tasks []Task
+	tasks   []Task
+	manager *Manager
 }
 
 func NewApplication(tasks ...Task) *Application {
 	return &Application{
-		tasks: tasks,
+		tasks:   tasks,
+		manager: NewManager(),
 	}
-}
-
-func createTask(ctx context.Context, task Task, action string, taskFunc func(Task, context.Context) error) func() error {
-	return func() (err error) {
-		defer func() {
-			if r := recover(); r != nil {
-				log.Errorw(
-					fmt.Sprintf("%s panic", action),
-					logfields.String("task", task.Identifier()),
-					logfields.Any("recover", r),
-				)
-				err = fmt.Errorf("%s %s panic: %v", action, task.Identifier(), r)
-			}
-		}()
-		err = taskFunc(task, ctx)
-		if err != nil {
-			log.Errorw(
-				fmt.Sprintf("%s error", action),
-				logfields.String("task", task.Identifier()),
-				logfields.Error(err),
-			)
-		}
-		return
-	}
-}
-
-func (a *Application) executeTasks(ctx context.Context, action string, stopOnError bool, taskFunc func(Task, context.Context) error) error {
-	wg, ctx := errgroup.WithContext(ctx)
-	for _, task := range a.tasks {
-		// 如果设置了stopOnError且上下文已取消，则跳过后续任务
-		if stopOnError && ctx.Err() != nil {
-			log.Infof("skip %s %s due to context error: %v", action, task.Identifier(), ctx.Err())
-			continue
-		}
-		log.Infof("%s %s", action, task.Identifier())
-		wg.Go(createTask(ctx, task, action, taskFunc))
-	}
-	return wg.Wait()
 }
 
 func (a *Application) Identifier() string {
@@ -68,15 +28,15 @@ func (a *Application) Identifier() string {
 }
 
 func (a *Application) Start(ctx context.Context) error {
-	// run 操作会因为一个任务失败而中断
-	return a.executeTasks(ctx, "start", true, func(t Task, ctx context.Context) error {
-		return t.Start(ctx)
-	})
+	for i, task := range a.tasks {
+		err := a.manager.RunTask(ctx, fmt.Sprintf("%d:%s", i, task.Identifier()), task)
+		if err != nil {
+			return err
+		}
+	}
+	return a.manager.Wait()
 }
 
 func (a *Application) Stop(ctx context.Context) error {
-	// close 操作不会因为一个任务失败而中断
-	return a.executeTasks(ctx, "stop", false, func(t Task, ctx context.Context) error {
-		return t.Stop(ctx)
-	})
+	return a.manager.Stop(ctx)
 }
---
+++ utils/boot/manager.go
@@ -0,0 +1,170 @@
+package boot
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/TBXark/sphere/log"
+	"github.com/TBXark/sphere/log/logfields"
+	"sync"
+)
+
+var (
+	ErrTaskAlreadyExists = errors.New("task already exists")
+	ErrTaskNotFound      = errors.New("task not found")
+)
+
+type Manager struct {
+	mu           sync.RWMutex
+	tasks        map[string]Task
+	runningErrs  ErrCollection
+	runningGroup sync.WaitGroup
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		tasks: make(map[string]Task),
+	}
+}
+
+func (m *Manager) RunTask(ctx context.Context, name string, task Task) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if _, ok := m.tasks[name]; ok {
+		return ErrTaskAlreadyExists
+	}
+	m.tasks[name] = task
+	m.runningGroup.Add(1)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				logTaskPanic(task, name, r)
+			}
+		}()
+		defer func() {
+			m.mu.Lock()
+			delete(m.tasks, name)
+			m.mu.Unlock()
+		}()
+		defer m.runningGroup.Done()
+		log.Infof("<Manager> %s starting", name)
+		err := task.Start(ctx)
+		if err != nil {
+			logTaskError(task, name, err)
+			m.runningErrs.add(err)
+			return
+		}
+		log.Infof("<Manager> %s finished", name)
+	}()
+	return nil
+}
+
+func (m *Manager) StopTask(ctx context.Context, name string) error {
+	m.mu.Lock()
+	task, ok := m.tasks[name]
+	if !ok {
+		m.mu.Unlock()
+		return ErrTaskNotFound
+	}
+	delete(m.tasks, name)
+	m.mu.Unlock()
+	log.Infof("<Manager> %s stopping", name)
+	err := task.Stop(ctx)
+	if err != nil {
+		return err
+	}
+	log.Infof("<Manager> %s stopped", name)
+	return nil
+}
+
+func (m *Manager) StopAll(ctx context.Context) error {
+	m.mu.Lock()
+	tasks := make(map[string]Task, len(m.tasks))
+	for name, task := range m.tasks {
+		tasks[name] = task
+		delete(m.tasks, name)
+	}
+	m.mu.Unlock()
+
+	var stopGroup sync.WaitGroup
+	var stopErrs ErrCollection
+	for name, task := range tasks {
+		stopGroup.Add(1)
+		go func(name string, task Task) {
+			defer func() {
+				if r := recover(); r != nil {
+					logTaskPanic(task, name, r)
+				}
+			}()
+			defer stopGroup.Done()
+			log.Infof("<Manager> %s stopping", name)
+			if err := task.Stop(ctx); err != nil {
+				logTaskError(task, name, err)
+				stopErrs.add(err)
+			}
+			log.Infof("<Manager> %s stopped", name)
+		}(name, task)
+	}
+	m.runningGroup.Wait()
+	return stopErrs.err()
+}
+
+func (m *Manager) Identifier() string {
+	return "task_manager"
+}
+
+func (m *Manager) Start(ctx context.Context) error {
+	return nil
+}
+
+func (m *Manager) Stop(ctx context.Context) error {
+	return m.StopAll(ctx)
+}
+
+func (m *Manager) Wait() error {
+	m.runningGroup.Wait()
+	return m.runningErrs.err()
+}
+
+func (m *Manager) IsRunning(name string) bool {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	_, ok := m.tasks[name]
+	return ok
+}
+
+type ErrCollection struct {
+	mu   sync.Mutex
+	errs []error
+}
+
+func (ec *ErrCollection) add(err error) {
+	ec.mu.Lock()
+	defer ec.mu.Unlock()
+	ec.errs = append(ec.errs, err)
+}
+
+func (ec *ErrCollection) err() error {
+	ec.mu.Lock()
+	defer ec.mu.Unlock()
+	if len(ec.errs) == 0 {
+		return nil
+	}
+	return errors.Join(ec.errs...)
+}
+
+func logTaskPanic(task Task, name string, reason any) {
+	log.Errorw(
+		fmt.Sprintf("%s panic", name),
+		logfields.String("task", task.Identifier()),
+		logfields.Any("recover", reason),
+	)
+}
+
+func logTaskError(task Task, name string, err error) {
+	log.Errorw(
+		fmt.Sprintf("%s error", name),
+		logfields.String("task", task.Identifier()),
+		logfields.Error(err),
+	)
+}
