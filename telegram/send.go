038ae5355b58cb6a24004c9792d26e72df7bcf63
tbxark <tbxark@outlook.com>
2025-05-17 23:25:26 +0800

    feat: add BroadcastMessage function for rate-limited message sending
    Signed-off-by: tbxark <tbxark@outlook.com>

--- telegram/send.go
+++ telegram/send.go
@@ -3,6 +3,7 @@ package telegram
 import (
 	"context"
 	"github.com/go-telegram/bot"
+	"golang.org/x/time/rate"
 )
 
 func SendMessage(ctx context.Context, b *bot.Bot, update *Update, m *Message) error {
@@ -57,4 +58,28 @@ func SendErrorMessage(ctx context.Context, b *bot.Bot, update *Update, err error
 			Text:            err.Error(),
 		})
 	}
+}
+
+func BroadcastMessage[T any](ctx context.Context, b *bot.Bot, data []T, rateLimiter *rate.Limiter, send func(context.Context, *bot.Bot, T) error, progress func(int, int)) error {
+	total := len(data)
+	for i, d := range data {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		if progress != nil {
+			progress(i, total)
+		}
+		err := rateLimiter.Wait(ctx)
+		if err != nil {
+			return err
+		}
+		err = send(ctx, b, d)
+		if err != nil {
+			return err
+		}
+	}
+	if progress != nil {
+		progress(total, total)
+	}
+	return nil
 }
