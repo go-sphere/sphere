418b189da4595c2324790733ae66c815f2a14478
tbxark <tbxark@outlook.com>
2025-07-15 16:32:52 +0800

    refactor: add transaction options with support for isolation level, read-only mode, commit and rollback hooks, and update `WithTx` and `WithTxEx` to support configurable options
    Signed-off-by: tbxark <tbxark@outlook.com>

--- layout/internal/pkg/dao/tx.go
+++ layout/internal/pkg/dao/tx.go
@@ -2,23 +2,77 @@ package dao
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/TBXark/sphere/layout/internal/pkg/database/ent"
 	"github.com/TBXark/sphere/log"
 	"github.com/TBXark/sphere/log/logfields"
 )
 
-func WithTx[T any](ctx context.Context, db *ent.Client, exe func(ctx context.Context, tx *ent.Client) (*T, error)) (*T, error) {
-	tx, tErr := db.BeginTx(ctx, nil)
-	if tErr != nil {
-		return nil, tErr
+type txOptions struct {
+	Isolation    sql.IsolationLevel
+	ReadOnly     bool
+	CommitHook   ent.CommitHook
+	RollbackHook ent.RollbackHook
+}
+
+type Option func(*txOptions)
+
+func WithTxIsolation(level sql.IsolationLevel) Option {
+	return func(opts *txOptions) {
+		opts.Isolation = level
+	}
+}
+
+func WithTxReadOnly(readOnly bool) Option {
+	return func(opts *txOptions) {
+		opts.ReadOnly = readOnly
+	}
+}
+
+func WithTxCommitHook(hook ent.CommitHook) Option {
+	return func(opts *txOptions) {
+		opts.CommitHook = hook
+	}
+}
+
+func WithTxRollbackHook(hook ent.RollbackHook) Option {
+	return func(opts *txOptions) {
+		opts.RollbackHook = hook
+	}
+}
+
+func newTxOptions(opts ...Option) *txOptions {
+	txOpts := &txOptions{
+		Isolation:    sql.LevelDefault,
+		ReadOnly:     false,
+		CommitHook:   nil,
+		RollbackHook: nil,
+	}
+	for _, opt := range opts {
+		opt(txOpts)
+	}
+	return txOpts
+}
+
+func WithTx[T any](ctx context.Context, db *ent.Client, exe func(ctx context.Context, tx *ent.Client) (*T, error), opts ...Option) (*T, error) {
+	txOpts := newTxOptions(opts...)
+	tx, err := db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: txOpts.Isolation,
+		ReadOnly:  txOpts.ReadOnly,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if txOpts.CommitHook != nil {
+		tx.OnCommit(txOpts.CommitHook)
+	}
+	if txOpts.RollbackHook != nil {
+		tx.OnRollback(txOpts.RollbackHook)
 	}
 	defer func() {
-		if e := recover(); e != nil {
-			log.Warnw(
-				"WithTx panic",
-				logfields.Any("error", e),
-			)
+		if reason := recover(); reason != nil {
+			log.Warnw("WithTx panic", logfields.Any("error", reason))
 			_ = tx.Rollback()
 			return
 		}
@@ -36,22 +90,29 @@ func WithTx[T any](ctx context.Context, db *ent.Client, exe func(ctx context.Con
 	return result, err
 }
 
-func WithTxEx(ctx context.Context, db *ent.Client, exe func(ctx context.Context, tx *ent.Client) error) error {
-	tx, tErr := db.BeginTx(ctx, nil)
-	if tErr != nil {
-		return tErr
+func WithTxEx(ctx context.Context, db *ent.Client, exe func(ctx context.Context, tx *ent.Client) error, opts ...Option) error {
+	txOpts := newTxOptions(opts...)
+	tx, err := db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: txOpts.Isolation,
+		ReadOnly:  txOpts.ReadOnly,
+	})
+	if err != nil {
+		return err
+	}
+	if txOpts.CommitHook != nil {
+		tx.OnCommit(txOpts.CommitHook)
+	}
+	if txOpts.RollbackHook != nil {
+		tx.OnRollback(txOpts.RollbackHook)
 	}
 	defer func() {
-		if e := recover(); e != nil {
-			log.Warnw(
-				"WithTxEx panic",
-				logfields.Any("error", e),
-			)
+		if reason := recover(); reason != nil {
+			log.Warnw("WithTxEx panic", logfields.Any("error", reason))
 			_ = tx.Rollback()
 			return
 		}
 	}()
-	err := exe(ctx, tx.Client())
+	err = exe(ctx, tx.Client())
 	if err != nil {
 		if rErr := tx.Rollback(); rErr != nil {
 			return rErr
