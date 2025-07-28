72879a4a9c38e5a998eae394040b0ec9134d59cb
tbxark <tbxark@outlook.com>
2025-07-28 14:30:55 +0800

    feat: refactor binding functions to use newUniverseDecoder and streamline binding methods
    Signed-off-by: tbxark <tbxark@outlook.com>

--- server/ginx/binding.go
+++ server/ginx/binding.go
@@ -10,19 +10,14 @@ import (
 )
 
 var (
-	urlBinding   = newDecoder("uri")
-	queryBinding = newDecoder("form")
+	urlBinding   = newUniverseDecoder("uri")
+	queryBinding = newUniverseDecoder("form")
 )
 
-func newDecoder(key string) *form.Decoder {
+func newUniverseDecoder(key string) *form.Decoder {
 	decoder := form.NewDecoder()
 	decoder.SetTagName(key)
-	decoder.RegisterTagNameFunc(tagNameFunc(key))
-	return decoder
-}
-
-func tagNameFunc(key string) func(field reflect.StructField) string {
-	return func(field reflect.StructField) string {
+	decoder.RegisterTagNameFunc(func(field reflect.StructField) string {
 		tags, err := structtag.Parse(string(field.Tag))
 		if err != nil {
 			return ""
@@ -38,7 +33,20 @@ func tagNameFunc(key string) func(field reflect.StructField) string {
 			}
 		}
 		return ""
-	}
+	})
+	return decoder
+}
+
+func ShouldBindUri(ctx *gin.Context, obj any) error {
+	return ctx.ShouldBindUri(obj)
+}
+
+func ShouldBindQuery(ctx *gin.Context, obj any) error {
+	return ctx.ShouldBindQuery(obj)
+}
+
+func ShouldBindJSON(ctx *gin.Context, obj any) error {
+	return ctx.ShouldBindJSON(obj)
 }
 
 func ShouldUniverseBindUri(ctx *gin.Context, obj any) error {
@@ -51,18 +59,6 @@ func ShouldUniverseBindUri(ctx *gin.Context, obj any) error {
 
 func ShouldUniverseBindQuery(ctx *gin.Context, obj any) error {
 	return queryBinding.Decode(obj, ctx.Request.URL.Query())
-}
-
-func ShouldBindQuery(ctx *gin.Context, obj any) error {
-	return ctx.ShouldBindQuery(obj)
-}
-
-func ShouldBindUri(ctx *gin.Context, obj any) error {
-	return ctx.ShouldBindUri(obj)
-}
-
-func ShouldBindJSON(ctx *gin.Context, obj any) error {
-	return ctx.ShouldBindJSON(obj)
 }
 
 func ShouldUniverseBind(ctx *gin.Context, obj any, uri, query, body bool) error {
